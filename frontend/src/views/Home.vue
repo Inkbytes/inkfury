1b6df23b70107d496668eeb00287677fcc05ca58
teslazap <oel-ouar@student.1337.ma>
2022-03-11 11:33:25 +0100

    frontend

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1 id="hello">Hello InkFury</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: { },
+});
+</script>
