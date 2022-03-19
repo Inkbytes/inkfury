801087225f521e98044f57eef17c3b0b564a174c
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-03-19 19:21:18 +0100

    KHDAAAAAM || WORKING ON ADDING ROOMS AND GROUPS

--- chat/socket-backend/src/app.module.ts
+++ chat/socket-backend/src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'static'),
+      rootPath: join(__dirname, '../..', 'static'),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService, ChatGateway, ChatGateway],
+  providers: [ChatGateway, ChatGateway],
 })
 export class AppModule {}
--- chat/socket-backend/src/chat.gateway.ts
+++ chat/socket-backend/src/chat.gateway.ts
@@ -2,9 +2,13 @@ import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessa
 import { Logger } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
+// This decorator gives us access to the socket.io functionality.
+@WebSocketGateway({
+  cors: true
+})
 export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
+  // This decorator gives us access to the websockets server instance.
   @WebSocketServer() wss: Server;
 
   private logger: Logger = new Logger('AppGateway');
@@ -12,19 +16,21 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   afterInit(server: Server) {
     this.logger.log('Server initialized!');
   }
-
   handleConnection(client: Socket, ...args: any[]) {
       this.logger.log(`'Client connected:    ${client.id}'`)
   }
-
   handleDisconnect(client: Socket) {
       this.logger.log(`'Client disconnected: ${client.id}'`)
   }
 
+  /* -- We make use of the instance in here, where we send data to all clients
+    connected to the server using the emit() function.
+    handleMessage uses the SubscribeMessage decorator which makes it able to
+    listen to an event named msgToServer.
+  */
   @SubscribeMessage('msgToServer')
-  handleMessage(client: any, data: string): void {
-    this.wss.emit(client.id)
-    this.wss.emit('msgToClient', `${client.id} -> ${data}`); // To Broadcast for example
+  handleMessage(client: Socket, payload: any): void {
+    this.wss.emit('msgToClient', payload);
     // client.emit('msgToClient', data);
   }
 }
