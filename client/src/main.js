64ef16b2bf69f8c256f8a60b842c421b8c24f760
Zakaria Taouil <ztaouil@student.1337.ma>
2022-03-20 11:00:16 +0100

    modified:   client/src/main.js 	new file:   server/run.sh 	modified:   server/src/GameGateway/event.gateway.ts 	modified:   server/src/main.ts

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,7 @@
-// enum game_status;
+//
+// main.js
+//
+// enum game_state;
 //
 // lobby.
 // pregame.
@@ -6,56 +9,46 @@
 // postgame.
 // quitgame.
 
-
-const socket = io("http://10.12.1.4:3000");
-
-let player = 0;
-let hamid = false;
+const socket = io("http://10.12.1.5:5000");
 
 socket.on("connect", ()=>{
 	console.log(`connected with id ${socket.id}`);
-	console.log(socket);
 })
 
-socket.on("initiation", (nb) => {
-	if (nb === 1){
-		player = 1;
-	}else if (nb === 2){
-		player = 2;
-		socket.emit('player2-connected', 1);
-	}
-});
+const lobby = Symbol('lobby');
+const pregame = Symbol('pregame');
+const game = Symbol('game');
+const postgame = Symbol('postgame');
+const quitgame = Symbol('quitgame');
 
+let game_state = lobby;
 
+let player_number = 0;
+let hamid = false;
+const maxWIDTH = 1200;
+const maxHEIGHT = 800;
+let WIDTH = 1200;
+let HEIGHT = 800;
+
+let list = [-4, -3, 3, 4];
+let xpos = WIDTH / 2;
+let ypos = HEIGHT / 2;
+let dx = list[Math.floor(Math.random() * 4)];
+let dy = list[Math.floor(Math.random() * 4)];
+let	p1pos = HEIGHT / 2;
+let p2pos = HEIGHT / 2;
+let p1score = 0;
+let p2score = 0;
+let	gameison = false;
+let connected_player_numbers = 0;
+
+let paddle_width = 10;
+let paddle_height = WIDTH / 10;
+let bg;
 
 const sketch = function(p){
-	const maxWIDTH = 1200;
-	const maxHEIGHT = 800;
-	let WIDTH = 1200;
-	let HEIGHT = 800;
-
-	let list = [-4, -3, 3, 4];
-	let xpos = WIDTH / 2;
-	let ypos = HEIGHT / 2;
-	let dx = list[Math.floor(Math.random() * 4)];
-	let dy = list[Math.floor(Math.random() * 4)];
-	let	p1pos = HEIGHT / 2;
-	let p2pos = HEIGHT / 2;
-	let p1score = 0;
-	let p2score = 0;
-	let	gameison = false;
-	let game_status = 0;
-	let connected_players = 0;
-
-	let paddle_width = 10;
-	let paddle_height = WIDTH / 10;
-	let bg;
-	let wait_lobby_peppe;
-	let welcome_tortoise;
 
 	p.preload = () => {
-		wait_lobby_peppe = p.loadImage('https://i.imgur.com/YVFcaI0.jpeg');
-		welcome_tortoise = p.loadImage('https://i.imgur.com/XCPkpZT.jpeg');
 	}
 
 	p.setup = () => {
@@ -64,29 +57,72 @@ const sketch = function(p){
 	};
 
 	let key_event = () => {
-		if (player == 1 && p.keyIsDown(p.UP_ARROW)){
+//	key event for both player_numbers
+		if (player_number == 1 && p.keyIsDown(p.UP_ARROW)){
 			if (p1pos > 0)
 				p1pos -= 10;
 		}
-		if (player == 1 && p.keyIsDown(p.DOWN_ARROW)){
+		if (player_number == 1 && p.keyIsDown(p.DOWN_ARROW)){
 			if (p1pos < HEIGHT - paddle_height)
 				p1pos += 10;
 		}
-		if (player == 2 && p.keyIsDown(p.UP_ARROW)){
+		if (player_number == 2 && p.keyIsDown(p.UP_ARROW)){
 			if (p2pos > 0)
 				p2pos -= 10;
 		}
-		if (player == 2 && p.keyIsDown(p.DOWN_ARROW)){
+		if (player_number == 2 && p.keyIsDown(p.DOWN_ARROW)){
 			if (p2pos < HEIGHT - paddle_height)
 				p2pos += 10;
 		}
 	}
+	let d_lobby = () => {
+// draw lobby
+		p.background(0);
+		p.stroke(1);
+		p.textSize(30);
+		p.fill('white');
+		p.text('Lobby', WIDTH / 2, HEIGHT / 2);
 
-	let	draw_objects = () => {
+// listen to 1or2-event to assign the number of player_number
+		socket.on('1or2-event', (message) => {
+			player_number = message;
+			game_state = pregame;
+//			console.log(message);
+		});
+	}
+
+	let d_pregame = () => {
+// draw pregame
+		p.background(0);
+		p.stroke(1);
+		p.textSize(30);
+		p.fill('white');
+		p.text('Pregame', WIDTH / 2, HEIGHT / 2);
+
+// hook space to start the game
+// emit startgame-event to notify the other player to start the game
+		if (p.keyIsDown(32)){
+			socket.emit('startgame-event', 1);
+			reset_game();
+			game_state = game;
+		}
+		socket.on('startgame-event', (data) => {
+			reset_game();
+			game_state = game;
+		});
+
+		socket.on('quitgame-event', (flag) => {
+			game_state = lobby;
+			socket.emit('queueme-event', 1);
+		});
+	}
+
+	let	d_game = () => {
+// draw game components
 		p.background(0);
 		p.stroke(1);
 		p.textSize(10);
-		p.text(player, 10, 10);
+		p.text(player_number, 10, 10);
 		p.strokeWeight(4);
 		p.stroke(1000);
 		p.line(WIDTH / 2, 0, WIDTH / 2, HEIGHT);
@@ -100,9 +136,70 @@ const sketch = function(p){
 		p.textSize(12);
 		p.text('PONG GAME', WIDTH / 2 - 37, 20);
 		p.fill('white');
+
+		key_event();
+		ball_collition_mouvement();
+
+		socket.on('quitgame-event', (flag) => {
+			console.log(`quitgame-event received`);
+			game_state = quitgame;
+		});
+		if (p1score == 5 || p2score == 5)
+			game_state = postgame;
 	}
 
-	let move_ball = () => {
+	let d_postgame = () => {
+// draw postgame
+		let quitflag = 0;
+		p.background(0);
+		p.stroke(1);
+		p.textSize(30);
+		p.fill('white');
+		let winner = (p1score > p2score) ? 1 : 2;
+		p.text('Postgame player_number ' + winner +
+			' won', WIDTH / 2, HEIGHT / 2);
+
+// reset game and replay
+		if (p.keyIsDown(32)){
+			if (!quitflag){
+				socket.emit('startgame-event', 1);
+				game_state = pregame;
+			}
+			else{
+				game_state = lobby;
+				socket.emit('queueme-event', 1);
+			}
+		}
+		socket.on('startgame-event', (data) => {
+			game_state = pregame;
+		});
+// somebody quit the game
+		socket.on('quitgame-event', (flag) => {
+			quitflag = flag;
+		});
+	}
+
+	let d_quitgame = () => {
+// draw quitgame
+		console.log('d_quitgame');
+		p.background(0);
+		p.stroke(1);
+		p.textSize(30);
+
+		let loser = (player_number === 1) ? 2 : 1;
+		p.text('player_number ' + loser + '  quits', WIDTH / 2, HEIGHT / 2);
+
+		if (p.keyIsDown(32))
+		{
+			console.log('spacebar pressed');
+			game_state = lobby;
+			socket.emit('queueme-event', 1);
+		}
+	}
+
+	let ball_collition_mouvement = () => {
+		let goal = false;
+//	collition with right and left edge goal or not
 		if (xpos + 10 >= WIDTH - paddle_width - 10 && ypos >= p2pos - 5 && ypos <= p2pos + paddle_height + 5)
 		{
 			dx > 0 ? dx += 1: dx -= 1;
@@ -111,6 +208,7 @@ const sketch = function(p){
 		else if (xpos + 10 >= WIDTH){
 			p1score += 1;
 			reset_ball();
+			goal = true;
 		}
 		if (xpos - 10 <= paddle_width + 10 && ypos >= p1pos - 5 && ypos <= p1pos + paddle_height + 5)
 		{
@@ -120,116 +218,80 @@ const sketch = function(p){
 		else if (xpos - 10 <= 0){
 			p2score += 1;
 			reset_ball();
+			goal = true;
 		}
+
+//	collition with top and bottom edge
 		if (ypos >= HEIGHT - 10 || ypos <= 10)
 		{
 			dy = -dy;
 		}
 
 
-		if (player == 1){
+//	move ball
+		if (player_number == 1){
 			xpos = xpos + dx;
 			ypos = ypos + dy;
 		}
+
+		if (player_number === 1 && goal === true)
+		{
+			socket.emit('scoregame-event', {p1: p1score, p2: p2score});
+		}
+		else{
+			socket.on('scoregame-event', (obj) => {
+				p1score = obj.p1;
+				p2score = obj.p2;
+			});
+		}
+		send_receiv_game_info();
+
+//	display score
 		p.textSize(100);
 		p.stroke('white');
 		p.text(p1score, (WIDTH / 2) - 150, HEIGHT / 2 - 150);
 		p.text(p2score, (WIDTH / 2) + 100, HEIGHT / 2 - 150);
 	}
-	let	reset_ball = () => { xpos = WIDTH / 2;
+
+
+	let	reset_ball = () => {
+//	reset ball to the middle of the court and reset dx & dy
+		xpos = WIDTH / 2;
 		ypos = HEIGHT / 2;
 		dx = list[Math.floor(Math.random() * 4)];
 		dy = list[Math.floor(Math.random() * 4)];
 	}
 
 	let reset_game = () => {
+// reset score and ball
+		reset_ball();
 		p1score = 0;
 		p2score = 0;
 	}
 
-// welcome page
-	let welcome_page = (n) => {
-		p.stroke(1);
-		p.textSize(10);
-		p.text(player, 10, 10);
-		p.stroke(1);
-		p.fill('white');
-		if (n === 0){
-			p.textSize(100);
-			p.text('WELCOME', WIDTH / 2 - 250, 100);
-			p.textSize(20);
-		}else if (n === 1){
-			p.textSize(40);
-			p.text('player 1 won', WIDTH / 2, 200);
-		}else if (n === 2){
-			p.textSize(40);
-			p.text('player 2 won', WIDTH / 2, 200);
-		}
-		if (player != 0){
-			if (player == 1){
-				socket.on("player2-connected", (msg) => {
-					connected_players = msg;
-					hamid = true;
-					socket.emit('ready', 1);
-				});
-			}
-			if (player === 2 || hamid === true){
-//				p.text('player connected press space to play', WIDTH / 2 - 150, 300);
-				p.image(welcome_tortoise, WIDTH / 2, HEIGHT / 2, 100, 100);
-				if (p.keyIsDown(32)){
-					reset_game();
-					gameison = true;
-				}
-			}
-			else{
-				p.image(wait_lobby_peppe, WIDTH / 2, HEIGHT / 2, 100, 100);
-			}
-		}
-	}
-
+	let send_receiv_game_info = () => {
+// send and receive game info
 //
+//	player 1 emits his paddle pos and the balls pos
+		if (player_number === 1)
+			socket.emit('game-event', {player: 1, paddle: p1pos / HEIGHT, ball: {x: xpos / WIDTH, y: ypos / HEIGHT}});
+//	player 2 emits his paddle pos
+		if (player_number === 2)
+			socket.emit('game-event', {player: 2, paddle: p2pos / HEIGHT});
 
-	let comunicate_coord = () => {
-		if (player == 1)
-		{
-			socket.emit("game", {player: 1,paddle: p1pos/HEIGHT, ball: {x: xpos/WIDTH, y: ypos/HEIGHT}});
-		}
-		else if (player == 2)
-		{
-			socket.emit("game", {player: 2,paddle: p2pos/HEIGHT});
-		}
-		socket.on("game", (obj) => {
-			if (obj.player == 1)
+//  player 1 receives player 2's paddle and player 2 receives p1 paddle's and ball
+		socket.on('game-event', (gameobj) => {
+			if (gameobj.player === 1)
 			{
-				p1pos = obj.paddle * HEIGHT;
-				xpos = obj.ball.x * WIDTH;
-				ypos = obj.ball.y * HEIGHT;
+				p1pos = gameobj.paddle * HEIGHT;
+				xpos = gameobj.ball.x * WIDTH;
+				ypos = gameobj.ball.y * HEIGHT;
 			}
-			else{
-				p2pos = obj.paddle * HEIGHT;
+			else
+			{
+				p2pos = gameobj.paddle * HEIGHT;
 			}
 		});
-	}
-
-	let debug_player = () => {
-		if (player == 1)
-			console.log("you are player number 1")
-		else if (player == 2)
-			console.log("you are player number 2")
-		else
-			console.log("you are a watcher");
-	}
-
-	let start_game = () => {
-		if (gameison){
-			socket.emit("start-game", "yalah");
-			reset_game();
-		}else{
-		socket.on("start-game", (data) => {
-			gameison = true;
-			reset_game();
-		});
-		}
 	}
 
 	p.windowResized = () => {
@@ -240,22 +302,16 @@ const sketch = function(p){
 	};
 
 	p.draw = function (){
-		if (p1score === 5){
-			gameison = false;
-			game_status = 1;
-		}else if (p2score === 5){
-			gameison = false;
-			game_status = 2;
-		}
-		if (gameison){
-			key_event();
-			draw_objects();
-			comunicate_coord();
-			move_ball();
-		}else{
-			welcome_page(game_status);
-			start_game();
-		}
+		if (game_state === lobby)
+			d_lobby();
+		else if (game_state === pregame)
+			d_pregame();
+		else if (game_state === game)
+			d_game();
+		else if (game_state === postgame)
+			d_postgame();
+		else if (game_state === quitgame)
+			d_quitgame();
 	};
 };
 
--- server/src/GameGateway/event.gateway.ts
+++ server/src/GameGateway/event.gateway.ts
@@ -1,35 +1,10 @@
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from "@nestjs/websockets";
 import { Logger } from '@nestjs/common';
 import { Server, Socket } from "socket.io";
-import { arrayBuffer } from "stream/consumers";
 
-// game class
-class Game{
-	player1Id: Socket;
-	player2Id: Socket;
-	watchersId: string[];
-	roomId: string;
-
-	constructor(p1: Socket, p2: Socket, room: string){
-		this.player1Id = p1;
-		this.player2Id = p2;
-		this.roomId = room;
-	}
-
-	addWatcher(watcherId: string){
-		this.watchersId.push(watcherId);
-	}
-
-	isPlayerInThisGame(id: Socket) : boolean {
-		if (this.player1Id == id || this.player2Id == id)
-			return true;
-		return false;
-	}
-}
-
-let connections1 = [];
-let gameNumber = 0;
-let GameArray: any [] = [];
+let io : any;
+let game_queue : any [] = [];
+let game_number : number = 0;
 
 @WebSocketGateway({ cors: true })
 export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect{
@@ -39,49 +14,82 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 
 	afterInit(server: Server){
 		this.logger.log('Server intialized..');
-		// console.log(server.sockets);
+		io = server;
 	}
 
 	handleConnection(client: Socket|any, ...args: any[]){
 		this.logger.log(`Client connected ${client.id}`);
-		connections1.push(client.id);
-		if (connections1.length == 1){
-			client.emit('initiation', 1);
-			client.join('room-'+gameNumber);
-			client.gameId = gameNumber;
-		}else if (connections1.length == 2){
-			client.emit('initiation', 2);
-			client.join('room-'+gameNumber);
-			client.gameId = gameNumber;
-			gameNumber++;
+		this.logger.log(`game_queue.length ${game_queue.length}`);
+		// add client to the queue
+		game_queue.push(client);
+		// if two players or more and on the queue arbitrary choose who is p1 and p2
+		// queue them advance to pregame status
+		if (game_queue.length >= 2)
+			queue_players();
+	}
+
+	handleDisconnect(client: Socket|any){
+		this.logger.log(`Client disconnected ${client.id}`);
+
+		if (game_queue.findIndex( (e: Socket|any) => e === client) != -1)
+			game_queue.splice(game_queue.findIndex( (e: Socket|any) => e === client), 1);
+		else{
+//	disconnected in pregame or game or postgame
+			const clients = io.sockets.adapter.rooms.get('room-'+client.gameId);
+
+			if (!clients)
+				return ;
+//	pay attention to who want to watch they won't be queued!
+			for (const e of clients)
+			{
+				const e_socket = io.sockets.sockets.get(e);
+				e_socket.leave('room-'+client.gameId);
+				e_socket.emit('quitgame-event', 1);
+			}
 		}
 	}
 
-	handleDisconnect(client: Socket){
-		this.logger.log(`Client disconnected ${client.id}`);
-		if (connections1.length >= 1)
-			connections1.splice(connections1.indexOf(client.id), 1);
-		this.logger.log(`Connections size: ${connections1.length}`);
+	@SubscribeMessage('startgame-event')
+	startgameHandler(client: Socket|any, data: any): void{
+		client.to('room-'+client.gameId).emit('startgame-event', 1);
 	}
 
-
-	@SubscribeMessage('game')
-	handleGame(client: Socket|any, data: any): void {
-		client.broadcast.to('room-'+client.gameId).emit('game', data);
+	@SubscribeMessage('game-event')
+	gameHandler(client: Socket|any, data: any): void{
+		client.to('room-'+client.gameId).emit('game-event', data);
 	}
 
-	@SubscribeMessage('start-game')
-	handleStartGame(client: Socket|any, data: any): void {
-		client.broadcast.to('room-'+client.gameId).emit('start-game', data);
+	@SubscribeMessage('scoregame-event')
+	scoreHandler(client: Socket|any, data: any): void{
+		client.to('room-'+client.gameId).emit('scoregame-event', data);
 	}
 
-	@SubscribeMessage('player2-connected')
-	handleP2(client: Socket|any, data: any): void{
-		client.broadcast.to('room-'+client.gameId).emit('player2-connected', data);
+	@SubscribeMessage('queueme-event')
+	queuemeHandler(client: Socket|any, data: any): void{
+		game_queue.push(client);
+		if (game_queue.length >= 2)
+			queue_players();
 	}
+}
 
-	@SubscribeMessage('ready')
-	ready_event(client:	Socket|any, data: any): void{
-		connections1.splice(0, connections1.length);
-	}
+let	queue_players = () => {
+	let player1: any = game_queue[0];
+	let player2: any = game_queue[1];
+
+	// emit to 1or2-event
+	player1.emit('1or2-event', 1);
+	player2.emit('1or2-event', 2);
+
+	// join the room game_number
+	player1.gameId = game_number;
+	player2.gameId = game_number;
+
+	player1.join('room-'+player1.gameId);
+	player2.join('room-'+player2.gameId);
+
+	// pop the players from the queue
+	game_queue.splice(0, 2);
+
+	// increment game_number
+	game_number++;
 }
