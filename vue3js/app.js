9f9d103e5ed6698965276ff04f7f1dc6fd29dc5a
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-03-22 19:03:56 +0100

    VUE COMPONENTS

--- vue3js/app.js
+++ vue3js/app.js
@@ -6,9 +6,17 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            autor: 'Zackaria Taouil',
-            age: '666',
+            url: "https://www.google.com",
+            // title: 'The Final Empire',
+            // autor: 'Zackaria Taouil',
+            // age: '666',
+            books: [
+                { title: 'Name of the Wind', author: 'Zack', img: 'assets/964430.jpeg', isFav: true },
+                { title: 'The word of the king', author: 'Za3im', img: 'assets/964430.jpeg', isFav: true },
+                { title: 'High status', author: 'Mashad', img: 'assets/964430.jpeg', isFav: true },
+            ],
+            /* x: 0,
+            y: 0 */
         }
     },
 
@@ -19,6 +27,30 @@ const app = Vue.createApp({
         },
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        toggleFavBook(book) {
+            console.log(book.isFav);
+            book.isFav = !book.isFav;
+            // this.book.isFav = !this.book.isFav;
+            // console.log(this.books)
+            // this.book.isFav = !this.book.isFav;
+            // if (this.book.isFav)
+            // this.books[book].isFav = !this.books[book].isFav;
+        }
+        /* handleEvent(e, data) {
+            console.log(e, e.type);
+            if (data)
+                console.log(data);
+        }, */
+        /* handleMousemove(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+        } */
+    },
+    
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav);
         }
     }
 })
