57463484042dc07fe9ec6c19c32c8defb9ea6c0b
f0rkr <mashad@student.1337.ma>
2022-03-31 14:41:04 +0000

    Adding routes for games and users.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger} from "@nestjs/common";
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 const port = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(port);
 }
-bootstrap().then(r =>{
+bootstrap().then(() => {
   Logger.log(`Server running on http://localhost:${port}`, 'Boostrap');
 });
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -3,22 +3,30 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '../../src/entities/user.entity';
 import { Repository } from 'typeorm';
 import { UserDto } from './dto/add-user.dto';
-import {User} from "./interfaces/user.interface";
+import { User } from './interfaces/user.interface';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(UserEntity) private readonly repo: Repository<UserEntity>) {
-    }
+  constructor(
+    @InjectRepository(UserEntity) private readonly repo: Repository<UserEntity>,
+  ) {}
 
-    public async getAll() {
-        return await this.repo.find();
-    }
+  public async getAll() {
+    return await this.repo.find();
+  }
 
-    public async create(user : UserDto) {
-        return await this.repo.save(user);
-    }
+  public async getUser(user: UserDto) {
+    return await this.repo.findOne({ login: user.login });
+  }
+  public async create(user: UserDto) {
+    return await this.repo.save(user);
+  }
 
-    public async remove(username : string) {
-        return await this.repo.delete({login: username});
-    }
+  public async remove(username: string) {
+    return await this.repo.delete({ login: username });
+  }
+
+  public async update(user: UserDto) {
+    return await this.repo.update({ id: user.id }, user);
+  }
 }
