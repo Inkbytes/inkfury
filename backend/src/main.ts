8e64c985dccd9aaa9a9635e5a6e10b4d692bbda7
Zakaria Taouil <ztaouil@student.1337.ma>
2022-03-31 14:54:43 +0000

    modified:   main.ts 	modified:   oauth/oauth.controller.ts 	modified:   oauth/oauth.module.ts 	modified:   oauth/oauth.service.ts 	update oauth module with the one in frontend.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger} from "@nestjs/common";
+import * as cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: "http://10.12.1.6:8081",
+    credentials: true
+  });
   await app.listen(port);
 }
-bootstrap().then(() => {
+bootstrap().then(r =>{
   Logger.log(`Server running on http://localhost:${port}`, 'Boostrap');
 });
--- backend/src/oauth/oauth.controller.ts
+++ backend/src/oauth/oauth.controller.ts
@@ -1,101 +1,79 @@
-import {
-  Controller,
-  Get,
-  Req,
-  Res,
-  Post,
-  UseGuards,
-  Headers,
-  UnauthorizedException,
-  ConsoleLogger,
-} from '@nestjs/common';
-import axios from 'axios';
+import { Controller, Get , Req, Res, Post, UseGuards, Headers, UnauthorizedException, ConsoleLogger} from '@nestjs/common';
+import axios from "axios";
 import { Request, response, Response } from 'express';
 import { OauthService } from './oauth.service';
 
+
 @Controller('login')
 export class OauthController {
-  constructor(private readonly authService: OauthService) {}
-
-  @Get()
-  async(@Res() res) {
-    return res.redirect(
-      'https://api.intra.42.fr/oauth/authorize?client_id=3f629c13c719f75d1671989b3a96bb75d7796453b81b6ca8096945afbb88ab9c&redirect_uri=http%3A%2F%2Flocalhost%3A9000%2Fapi%2Flogin%2Fintra%2Fredirect&response_type=code',
-    );
+  constructor(private readonly authService: OauthService) {
   }
 
+
+  @Get()
+  async (@Res() res) {
+    return res.redirect("https://api.intra.42.fr/oauth/authorize?client_id=692c512c7ac5c517fff90c0360ad71d0ebb322b9c5b2bd373cf01102e2836fe5&redirect_uri=http%3A%2F%2F10.12.1.6%3A9000%2Fapi%2Flogin%2Fintra%2Fredirect&response_type=code");
+  }
+  
+
   @Get('/intra/redirect')
-  async IntraAuthRedirect(@Req() req, @Res() res): Promise<any> {
+  async IntraAuthRedirect(@Req() req, @Res() res) : Promise<any> {
     console.log(req.query.code);
     if (req.query.code === undefined) {
       return res.status(401).redirect('http://10.12.1.6:8081/');
     }
-
+   
     res.cookie('oauth2_grant_code', req.query.code);
-    return res.redirect(`http://localhost:9000/api/chat`);
+    return res.redirect(`http://10.12.1.6:8081/?auth=true`);
   }
 
-  @Post('/login_verification')
-  async loginVerification(
-    @Req() req: Request,
-    @Res() res: Response,
-    @Headers() headers,
-  ): Promise<any> {
-    console.log(req);
-    if (
-      !req.body.cookies['oauth2_grant_code'] &&
-      !req.body.cookies['access_token']
-    )
-      throw new UnauthorizedException();
-    else if (req.body.cookies['access_token']) {
-      const result = await axios({
-        url: 'https://api.intra.42.fr/v2/me',
-        method: 'GET',
-        headers: {
-          Authorization: 'Bearer ' + req.body.cookies['access_token'],
-        },
-      })
-        .then((resp) => {
-          return this.authService.GetUserData(
-            resp.data,
-            req.body.cookies['access_token'],
-          );
-        })
-        .catch((err) => {
-          console.log(err.message);
-          throw new UnauthorizedException();
-        });
 
+  @Post('/login_verification')
+  async loginVerification(@Req() req : Request, @Res() res : Response, @Headers() headers) : Promise<any> {
+
+    if (!req.cookies['oauth2_grant_code'] && !req.cookies['access_token'])
+      throw new UnauthorizedException();
+    else if (req.cookies['access_token']) {
+      const result = await axios({
+        url: "https://api.intra.42.fr/v2/me",
+        method: "GET",
+        headers: {
+          "Authorization": "Bearer " + req.cookies['access_token']
+        }
+      }).then(resp => {
+        return this.authService.GetUserData(resp.data, req.cookies['access_token']);
+      }).catch(err => {
+        console.log(err.message);
+        throw new UnauthorizedException();
+      })
+  
       return res.json(result);
     }
 
-    const code = req.body.cookies['oauth2_grant_code'];
-    const access_token = await this.authService
-      .GetAccessToken(code)
-      .then((res) => {
-        return res;
-      });
 
+    const code = req.cookies['oauth2_grant_code'];
+    const access_token = await this.authService.GetAccessToken(code).then((res) => {
+        return res;
+    });
+    
     console.log(access_token);
     // Set cookies
     res.cookie('access_token', access_token);
     res.clearCookie('oauth2_grant_code');
 
     const result = await axios({
-      url: 'https://api.intra.42.fr/v2/me',
-      method: 'GET',
+      url: "https://api.intra.42.fr/v2/me",
+      method: "GET",
       headers: {
-        Authorization: 'Bearer ' + access_token,
-      },
-    })
-      .then((resp) => {
+        "Authorization": "Bearer " + access_token
+      }
+    }).then(resp => {
         // Create new user
         return this.authService.GetUserData(resp.data, access_token);
-      })
-      .catch((err) => {
-        console.log(err.message);
-        throw new UnauthorizedException();
-      });
+    }).catch(err => {
+      console.log(err.message);
+      throw new UnauthorizedException();
+    })
 
     return res.json(result);
   }
