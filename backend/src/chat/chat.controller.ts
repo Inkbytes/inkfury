cb01827e527cf2eafc605cc64c04617d2aae7126
f0rkr <f0rkr42@gmail.com>
2022-04-05 16:06:35 +0000

    Protecting routes using jwt token.

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -1,13 +1,81 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Req,
+  Param,
+  Put,
+  UseGuards,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { User } from '../users/interfaces/user.interface';
+import { RoomDto } from './dto/chat.dto';
 import { ChatService } from './chat.service';
+import { Request } from 'express';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('chat')
+// @UseGuards(AuthGuard)
 export class ChatController {
-    constructor(private readonly chatService : ChatService) {}
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly userService : UsersService,
+    private jwtService : JwtService
+  ){}
 
+  /* -- PROBLEM WOULD BE ROUTES -- */
 
-    @Get()
-    TestChat() {
-        return this.chatService.test();
+  /* @Get(':id')
+    async getRoom(@Param('id') id: number) {
+        return this.chatService.getRoom(id);
+    } */
+
+  @Get()
+  async getRooms() {
+
+    return this.chatService.getRooms();
+  }
+
+  @Get(':id/members')
+  async getRoomMembers(@Param('id') id: number) {
+    return this.chatService.getRoomMembers(id);
+  }
+
+  @Get(':id/blocked_members')
+  async getRoomBlockedMembers(@Param('id') id: number) {
+    return this.chatService.getRoomBlockedMembers(id);
+  }
+
+  @Get(':id/muted_members')
+  async getRoomMutedMembers(@Param('id') id: number) {
+    return this.chatService.getRoomMutedMembers(id);
+  }
+
+  @Post()
+  async createRoom(@Body() room: RoomDto) {
+    console.log(room);
+    return this.chatService.createRoom(room);
+  }
+
+  @Post(':id')
+    async updateRoom(@Req() req : Request, @Param('id') id: number, @Body() roomData: RoomDto) {
+      const cookie = req.cookies['jwt'];
+      if (!cookie) {
+        throw new UnauthorizedException();
+      }
+
+      const data = await this.jwtService.verifyAsync(cookie);
+      if (!data) {throw new UnauthorizedException();}
+
+      const currentUser = await this.userService.getUserById(data['id']);
+      return this.chatService.updateRoom(id, roomData, currentUser);
     }
+
+  @Delete(':id')
+  async deleteRoom(@Param('id') id: number) {
+    return this.chatService.deleteRoom(id);
+  }
 }
--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
-import { AlertController } from './alert/alert.controller';
-import { AlertGateway } from './alert/alert.gateway';
-
+import { ChatGateway } from './chat.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomEntity } from '../entities/chat.entity';
+import { UserEntity } from '../entities/user.entity';
 
 @Module({
-  providers: [ChatService, AlertGateway],
-  controllers: [ChatController, AlertController]
+  imports: [
+    TypeOrmModule.forFeature([RoomEntity]),
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
+  controllers: [ChatController],
+  providers: [ChatService, ChatGateway],
 })
 export class ChatModule {}
